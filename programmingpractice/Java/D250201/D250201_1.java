c4619bb3ed41fdc1574fc0c101eca571b5e306e7
Jeonghyun Kwak <chawoni21@naver.com>
2025-02-01 16:40:31 +0900

    Seventeenth commit

---
+++ programmingpractice/Java/D250201/D250201_1.java
@@ -0,0 +1,10 @@
+public class D250201_1 {
+    public static int solution(int[] num_list) {
+        int answer = 0;
+        return answer;
+    }   
+    
+    public static void main(String[] args) {
+        
+    }
+}
